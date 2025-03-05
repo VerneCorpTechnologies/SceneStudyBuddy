caef1ddd382d155dfa9deb122730e5af60ae857c
davidverne-jisc <david.verne@jisc.ac.uk>
2025-03-05 20:05:46 +0000

    downgraded typescript version

--- ui/src/index.js
+++ ui/src/index.js
@@ -1,7 +1,6 @@
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import * as serviceWorker from 'serviceWorker';
 import App from 'App';
 import { store } from 'store';
 import 'assets/scss/style.scss';
@@ -18,5 +17,3 @@ root.render(
     </BrowserRouter>
   </Provider>
 );
-
-serviceWorker.unregister();
